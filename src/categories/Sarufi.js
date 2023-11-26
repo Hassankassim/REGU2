d50b897689aa41b216a6496a454d1d71af682dd2
HASSAN KASSIM ELIAS <114056773+Hassankassim@users.noreply.github.com>
2023-11-26 13:06:34 +0300

    6

--- src/categories/Sarufi.js
+++ src/categories/Sarufi.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import Navbaruser from '../components/navbar/Navbaruser'
-import Footer from '../components/footer/footer'
-import SarufiChatbox from "react-sarufi-chatbox"
-import chat from '../assets/img/icons8-chat-bot-96.png'
+import React from "react";
+import Navbaruser from "../components/navbar/Navbaruser";
+import Footer from "../components/footer/footer";
+import SarufiChatbox from "react-sarufi-chatbox";
+import chat from "../assets/img/icons8-chat-bot-96.png";
 // usage
 
 function Sarufi() {
   return (
     <>
-    <Navbaruser />
-    <SarufiChatbox botId={2517} />
-    <div className='center margint'>Sarufi</div>
-    <h4 className='margintt'>Welcome to our Economybot</h4>
-   <div className='center'> <img  src={chat}/></div>
-    <p className="text red">Click Below to Economy Chatbot</p>
-    <Footer />
-   </>
-  )
+      <Navbaruser />
+      <SarufiChatbox botId={2517} />
+      <div className="center margint">Sarufi</div>
+      <h4 className="margintt">Welcome to our Economybot</h4>
+      <div className="center">
+        {" "}
+        <img src={chat} />
+      </div>
+      <p className="text red">
+        "Welcome to our innovative Economy Bot! 🤖 We're thrilled to introduce
+        you to your personal guide for real-time economic insights. This
+        friendly bot is designed to provide you with valuable market
+        information, assist in navigating economic trends, and support your
+        decision-making process. Ask any economic-related question, and our
+        Economy Bot is here to help you stay informed and empowered in your
+        business endeavors. Let's explore the world of economic insights
+        together!
+      </p>
+      <Footer />
+    </>
+  );
 }
 
-export default Sarufi
+export default Sarufi;
