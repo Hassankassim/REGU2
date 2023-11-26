1008ded4bd4105b601e8f1d353d0caf6b43a49c1
HASSAN KASSIM ELIAS <114056773+Hassankassim@users.noreply.github.com>
2023-11-26 12:26:34 +0300

    2

--- src/counts/count.js
+++ src/counts/count.js
@@ -1,41 +1,36 @@
 import React, { useEffect } from 'react';
-import Carduser from '../user/Carduser'
-const Count = () => {
-  
+import Carduser from '../user/Carduser';
+import image1 from '../assets/img/vcard icon2.png';
+
+function Count() {
   return (
     <section id="counts" className="counts section-bg margintt">
       <div className="container margint">
         <div className="row counters">
           <div className="col-lg-3 col-6 text-center">
-            <span
-             
-              className="purecounter"
-            ><Carduser /></span>
+            <span className="purecounter">
+              <Carduser image={image1} title="Roadmap" />
+            </span>
             <p>Roadmap</p>
           </div>
 
           <div className="col-lg-3 col-6 text-center">
-            <span
-         
-              className="purecounter"
-            ><Carduser details="" /></span>
+            <span className="purecounter">
+              <Carduser image={image1} title="Tax/License" />
+            </span>
             <p>Tax/License</p>
           </div>
 
           <div className="col-lg-3 col-6 text-center">
-            <span
-         
-              className="purecounter"
-            > <Carduser /></span>
+            <span className="purecounter">
+              <Carduser title="Economy Bot" />
+            </span>
             <p>Economy Bot</p>
           </div>
 
           <div className="col-lg-3 col-6 text-center">
-            <span
-             
-              className="purecounter"
-            >
-              <Carduser />
+            <span className="purecounter">
+              <Carduser title="Compliances checklists" />
             </span>
             <p>Compliances checklists</p>
           </div>
@@ -43,6 +38,6 @@ const Count = () => {
       </div>
     </section>
   );
-};
+}
 
 export default Count;
